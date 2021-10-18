004960ee74d0b4e3b5cf3dad08f60d76abbe8b6c
Kevin Malakoff <kmalakoff@gmail.com>
2021-10-17 19:46:21 -0700

    Refactor polyfills

--- test/lib/stringIterator.cjs
+++ test/lib/stringIterator.cjs
@@ -1,17 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const encodeUTF8 = require("./encodeUTF8.cjs");
+
 const hasIterator = typeof Symbol !== "undefined" && Symbol.asyncIterator;
-const hasBuffer = typeof Buffer !== "undefined";
-
-if (hasBuffer && !Buffer.alloc) {
-  Buffer.alloc = function alloc(length, data) {
-    let buffer = data;
-    while (--length > 0) buffer += data;
-    return new Buffer(buffer);
-  };
-}
-
-function from(s) {
-  return hasBuffer ? Buffer.alloc(1, s) : Uint8Array.from(s, (x) => x.charCodeAt(0));
-}
 
 module.exports = function stringIterator(string) {
   let offset = 0;
@@ -19,7 +9,7 @@ module.exports = function stringIterator(string) {
     next() {
       if (offset >= string.length) return Promise.resolve({ value: undefined, done: true });
       const s = string.substring(offset, ++offset);
-      return Promise.resolve({ value: from(s), done: false });
+      return Promise.resolve({ value: encodeUTF8(s), done: false });
     },
   };
 
