e16fd0e09644f7c6d80e06df099eed1cc89735b6
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-19 06:17:33 -0800

    Upgrade modules

--- test/exports/import.test.mjs
+++ test/exports/import.test.mjs
@@ -1,14 +1,13 @@
-import '../lib/polyfill.cjs';
 import assert from 'assert';
 import newlineIterator from 'newline-async-iterator';
-import stringIterator from '../lib/stringIterator.cjs';
+import stringIterator from '../lib/stringIterator.ts';
 
-describe('exports .mjs', function () {
-  it('first newline', function (done) {
+describe('exports .mjs', () => {
+  it('first newline', (done) => {
     const iterator = newlineIterator(stringIterator('some\r\nstring\ncombination\r'));
     iterator
       .next()
-      .then(function (next) {
+      .then((next) => {
         assert.deepEqual(next, { value: 'some', done: false });
         done();
       })
